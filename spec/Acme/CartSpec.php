8554da28bbee1ed30f0afcc1a0dc7bb70b97806f
Remco Meeuwissen <dearon@gmail.com>
2015-02-07 16:23:20 +0100

    Added tests and basic functionality for adding products to a cart

--- spec/Acme/CartSpec.php
+++ spec/Acme/CartSpec.php
@@ -5,10 +5,33 @@ namespace spec\Acme;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+use Acme\ProductRepository;
+
 class CartSpec extends ObjectBehavior
 {
+    private $productRepository;
+
+    function __construct()
+    {
+        $this->productRepository = new ProductRepository();
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Acme\Cart');
+    }
+
+    function it_can_receive_a_single_product()
+    {
+        $this->add($this->productRepository->find("Product"));
+        $this->get()->shouldHaveCount(1);
+    }
+
+    function it_can_receive_multiple_products()
+    {
+        $this->add($this->productRepository->find("Product 1"));
+        $this->add($this->productRepository->find("Product 2"));
+        $this->add($this->productRepository->find("Product 3"));
+        $this->get()->shouldHaveCount(3);
     }
 }
--- src/Acme/Cart.php
+++ src/Acme/Cart.php
@@ -4,4 +4,15 @@ namespace Acme;
 
 class Cart
 {
+    private $products = array();
+
+    public function add($product)
+    {
+        $this->products[] = $product;
+    }
+
+    public function get()
+    {
+        return $this->products;
+    }
 }
