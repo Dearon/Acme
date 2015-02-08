a722631b48f7fda9f5d9ee50300cad79f4215ad4
Remco Meeuwissen <dearon@gmail.com>
2015-02-08 16:20:04 +0100

    Added a Product entity and updated the phpspec test to work with the new model

--- spec/Acme/CartSpec.php
+++ spec/Acme/CartSpec.php
@@ -21,26 +21,49 @@ class CartSpec extends ObjectBehavior
         $this->shouldHaveType('Acme\Cart');
     }
 
-    function it_can_receive_a_single_product()
+    function it_can_receive_a_single_product($product)
     {
-        $this->add($this->productRepository->find("Product"));
+        $product->beADoubleOf('Acme\Product');
+        $product->getName()->willReturn('Product');
+        $product->getPrice()->willReturn('1.00');
+
+        $this->add($product);
         $this->get()->shouldHaveCount(1);
     }
 
-    function it_can_receive_multiple_products()
+    function it_can_receive_multiple_products($productOne, $productTwo)
     {
-        $this->add($this->productRepository->find("Product 1"));
-        $this->add($this->productRepository->find("Product 2"));
-        $this->add($this->productRepository->find("Product 3"));
-        $this->get()->shouldHaveCount(3);
+        $productOne->beADoubleOf('Acme\Product');
+        $productOne->getName()->willReturn('Product 1');
+        $productOne->getPrice()->willReturn('1.00');
+
+        $productTwo->beADoubleOf('Acme\Product');
+        $productTwo->getName()->willReturn('Product 2');
+        $productTwo->getPrice()->willReturn('1.00');
+
+        $this->add($productOne);
+        $this->add($productTwo);
+        $this->get()->shouldHaveCount(2);
     }
 
-    function it_can_remove_a_product()
+    function it_can_remove_a_product($productOne, $productTwo, $productThree)
     {
-        $this->add($this->productRepository->find("Product 1"));
-        $this->add($this->productRepository->find("Product 2"));
-        $this->add($this->productRepository->find("Product 3"));
-        $this->remove($this->productRepository->find("Product 1"));
+        $productOne->beADoubleOf('Acme\Product');
+        $productOne->getName()->willReturn('Product 1');
+        $productOne->getPrice()->willReturn('1.00');
+
+        $productTwo->beADoubleOf('Acme\Product');
+        $productTwo->getName()->willReturn('Product 2');
+        $productTwo->getPrice()->willReturn('1.00');
+
+        $productThree->beADoubleOf('Acme\Product');
+        $productThree->getName()->willReturn('Product 3');
+        $productThree->getPrice()->willReturn('1.00');
+
+        $this->add($productOne);
+        $this->add($productTwo);
+        $this->add($productThree);
+        $this->remove($productTwo);
         $this->get()->shouldHaveCount(2);
     }
 }
