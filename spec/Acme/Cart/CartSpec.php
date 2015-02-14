66a033df6a68330ed2b506b3814b97b6ea5cac33
Dearon <dearon@gmail.com>
2015-02-14 14:02:21 +0100

    Moved Acme\Cart to Acme\Cart\Cart and made some small tweaks to the mock return values

--- spec/Acme/Cart/CartSpec.php
+++ spec/Acme/Cart/CartSpec.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace spec\Acme;
+namespace spec\Acme\Cart;
 
-use Acme\Product\Product;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -10,13 +9,13 @@ class CartSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->shouldHaveType('Acme\Cart');
+        $this->shouldHaveType('Acme\Cart\Cart');
     }
 
     function it_can_receive_a_single_product(\Acme\Product\Product $product)
     {
         $product->getName()->willReturn('Product');
-        $product->getPrice()->willReturn('1.00');
+        $product->getPrice()->willReturn(1.00);
 
         $this->add($product);
         $this->get()->shouldHaveCount(1);
@@ -25,10 +24,10 @@ class CartSpec extends ObjectBehavior
     function it_can_receive_multiple_products(\Acme\Product\Product $productOne, \Acme\Product\Product $productTwo)
     {
         $productOne->getName()->willReturn('Product 1');
-        $productOne->getPrice()->willReturn('1.00');
+        $productOne->getPrice()->willReturn(1.00);
 
         $productTwo->getName()->willReturn('Product 2');
-        $productTwo->getPrice()->willReturn('1.00');
+        $productTwo->getPrice()->willReturn(1.00);
 
         $this->add($productOne);
         $this->add($productTwo);
@@ -38,13 +37,13 @@ class CartSpec extends ObjectBehavior
     function it_can_remove_a_product(\Acme\Product\Product $productOne, \Acme\Product\Product $productTwo, \Acme\Product\Product $productThree)
     {
         $productOne->getName()->willReturn('Product 1');
-        $productOne->getPrice()->willReturn('1.00');
+        $productOne->getPrice()->willReturn(1.00);
 
         $productTwo->getName()->willReturn('Product 2');
-        $productTwo->getPrice()->willReturn('1.00');
+        $productTwo->getPrice()->willReturn(1.00);
 
         $productThree->getName()->willReturn('Product 3');
-        $productThree->getPrice()->willReturn('1.00');
+        $productThree->getPrice()->willReturn(1.00);
 
         $this->add($productOne);
         $this->add($productTwo);
@@ -56,10 +55,10 @@ class CartSpec extends ObjectBehavior
     function it_can_show_the_total_price(\Acme\Product\Product $productOne, \Acme\Product\Product $productTwo)
     {
         $productOne->getName()->willReturn('Product 1');
-        $productOne->getPrice()->willReturn('0.50');
+        $productOne->getPrice()->willReturn(0.50);
 
         $productTwo->getName()->willReturn('Product 2');
-        $productTwo->getPrice()->willReturn('5.00');
+        $productTwo->getPrice()->willReturn(5.00);
 
         $this->add($productOne);
         $this->add($productTwo);
