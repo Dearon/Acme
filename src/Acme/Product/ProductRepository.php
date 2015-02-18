21c592454d7b91aea2b5094da8407b1592e1e998
Dearon <dearon@gmail.com>
2015-02-18 13:38:08 +0100

    Updated the repository to make it possible to find product entities

--- src/Acme/Product/ProductRepository.php
+++ src/Acme/Product/ProductRepository.php
@@ -4,4 +4,26 @@ namespace Acme\Product;
 
 class ProductRepository
 {
+    private $entityManager;
+
+    public function __construct(\Doctrine\ORM\EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    private function getRepository()
+    {
+        return $this->entityManager->getRepository("\Acme\Product\Product");
+    }
+
+    public function findAll()
+    {
+        return $this->getRepository()->findAll();
+    }
+
+    public function findByName($name)
+    {
+        $name = new \Acme\Product\Name($name);
+        return $this->getRepository()->findOneBy(array('name.name' => $name->getName()));
+    }
 }
