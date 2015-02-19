469f4b4fa82f6b8ba7a56cc64dcc95df7aa84c1c
Dearon <dearon@gmail.com>
2015-02-19 23:07:06 +0100

    Fixed a failing test due to not supplying the required entityManager

--- spec/Acme/Product/ProductRepositorySpec.php
+++ spec/Acme/Product/ProductRepositorySpec.php
@@ -7,6 +7,11 @@ use Prophecy\Argument;
 
 class ProductRepositorySpec extends ObjectBehavior
 {
+    function let(\Doctrine\ORM\EntityManager $entityManager)
+    {
+        $this->beConstructedWith($entityManager);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Acme\Product\ProductRepository');
