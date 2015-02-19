796d4b795d30d64525fa670968906e7b906cb3f6
Dearon <dearon@gmail.com>
2015-02-19 23:07:31 +0100

    Added tests for the slug generation

--- spec/Acme/Product/NameSpec.php
+++ spec/Acme/Product/NameSpec.php
@@ -9,7 +9,7 @@ class NameSpec extends ObjectBehavior
 {
     function let()
     {
-        $this->beConstructedWith('Product');
+        $this->beConstructedWith('Product 1');
     }
 
     function it_is_initializable()
@@ -29,6 +29,21 @@ class NameSpec extends ObjectBehavior
 
     function it_should_show_a_name()
     {
-        $this->getName()->shouldBe('Product');
+        $this->getName()->shouldBe('Product 1');
+    }
+
+    function it_should_generate_a_slug(\Acme\Product\ProductRepository $productRepository)
+    {
+        $productRepository->findBySlug('product-1')->willReturn(null);
+        $this->generateSlug($productRepository);
+        $this->getSlug()->shouldBe('product-1');
+    }
+
+    function it_should_not_generate_a_duplicate_slug(\Acme\Product\ProductRepository $productRepository)
+    {
+        $productRepository->findBySlug('product-1')->willReturn(true);
+        $productRepository->findBySlug('product-1-1')->willReturn(null);
+        $this->generateSlug($productRepository);
+        $this->getSlug()->shouldBe('product-1-1');
     }
 }
